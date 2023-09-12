d1b3f073342d34eb1503cf5a2f998af71fc8b101
serkanboyraci <serkanboyraci@gmail.com>
2023-09-12 09:58:00 +0200

    to learn gesture recognizers

--- GestureRecognizer/ViewController.swift
+++ GestureRecognizer/ViewController.swift
@@ -8,10 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    
+    var number = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true // to use tapped image.
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        // target VC nin kendisi yani self
+        
+        // to add gesture to imageView you have to use this style.
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
+        
+    }
+    
+    @objc func changePic() {
+        
+        if number == 0 {
+            imageView.image = UIImage(named: "haluklevent")
+            nameLabel.text = "Haluk LEVENT"
+            number += 1
+        } else if number == 1 {
+            imageView.image = UIImage(named: "tarkan")
+            nameLabel.text = "TARKAN"
+            number += 1
+        } else {
+            imageView.image = UIImage(named: "teoman")
+            nameLabel.text = "TEOMAN"
+            number -= 2
+        }
+        
+        
+        
     }
 
 
